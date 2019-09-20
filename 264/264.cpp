9d90e173502ebf13e31cb6a492252f9e9e545f97
Timur Turatbekov <timatur242005@gmail.com>
2019-09-20 22:14:56 +0300

    Solved 264

---
+++ 264/264.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int k;
+	cin >> k;
+
+	if (k == 1 || k == 2 || k == 4 || k == 7)
+	{
+		cout << "NO" << "\n";
+	}
+	else
+	{
+		cout << "YES" << "\n";
+	}
+}
