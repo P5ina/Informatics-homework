10f60fa741769eb8bbfe02dc8d11725f85cf2af2
Timur Turatbekov <timatur242005@gmail.com>
2019-09-20 22:44:44 +0300

    Solved 3060 and reorganized folders

---
+++ 3060/3060.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	int n, curNum = 1;
+	bool isSquare = false;
+	cin >> n;
+	while (curNum <= n)
+	{
+		if (n == curNum)
+		{
+			isSquare = true;
+		}
+		curNum *= 2;
+	}
+	if (isSquare)
+	{
+		cout << "YES" << "\n";
+	}
+	else
+	{
+		cout << "NO" << "\n";
+	}
+}
