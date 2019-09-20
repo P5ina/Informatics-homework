4d454788078a1343c89cb8002a2d5e856b49b1e6
Timur Turatbekov <timatur242005@gmail.com>
2019-09-20 18:33:13 +0300

    Solved 259

---
+++ 259/259.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int k;
+	cin >> k;
+
+	if (k % 4 == 0 || k == 1)
+	{
+		cout << "YES" << "\n";
+	}
+	else
+	{
+		cout << "NO" << "\n";
+	}
+    
+}
