929bb6c7e1659543edc5edf9fc9bec1bad3dd692
Timur Turatbekov <timatur242005@gmail.com>
2019-09-21 07:26:11 +0300

    Solved 112215

---
+++ 112215/112215.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n, prevNum;
+	bool isRepeats = true;
+	cin >> n;
+
+	prevNum = n % 10;
+
+	while (n > 0)
+	{
+		if (prevNum != n % 10)
+		{
+			isRepeats = false;
+		}
+		prevNum = n % 10;
+		n /= 10;
+	}
+	if (isRepeats)
+	{
+		cout << "YES" << "\n";
+	}
+	else
+	{
+		cout << "NO" << "\n";
+	}
+
+	return 0;
+}
