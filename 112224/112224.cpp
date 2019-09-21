d823edc3b4c444c676b48668a99d23840f6840a9
Timur Turatbekov <timatur242005@gmail.com>
2019-09-21 07:42:59 +0300

    Solved 112224

---
+++ 112224/112224.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n, numSum = 1;
+	cin >> n;
+
+	for (int i = 1; i <= n; i++)
+	{
+		numSum *= i;
+	}
+    cout << numSum << "\n";
+}
