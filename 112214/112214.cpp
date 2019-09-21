3cde0d5c4a97ec494e3665b9b58843a58e83085f
Timur Turatbekov <timatur242005@gmail.com>
2019-09-21 07:21:45 +0300

    Solved 112214

---
+++ 112214/112214.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n, prevNum = -1;
+	bool hasDoubles = false;
+	cin >> n;
+
+	while (n > 0)
+	{
+		if (prevNum == n % 10)
+		{
+			hasDoubles = true;
+		}
+		prevNum = n % 10;
+		n /= 10;
+	}
+	if (hasDoubles)
+	{
+		cout << "YES" << "\n";
+	}
+	else
+	{
+		cout << "NO" << "\n";
+	}
+
+	return 0;
+}
