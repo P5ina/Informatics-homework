7428f149c674a759e7c949609e1f3d5480353092
Timur Turatbekov <timatur242005@gmail.com>
2019-09-22 23:56:43 +0300

    Solved 112172 and 112173

---
+++ 112172/112172.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	double x, y;
+	cin >> x >> y;
+
+	if (x >= 0 && y <= 1 && (y >= x - 1 || x * x + y * y <= 1))
+	{
+		cout << "YES" << endl;
+	}
+	else
+	{
+		cout << "NO" << endl;
+	}
+}
+
---
+++ 112173/112173.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	double x, y;
+	cin >> x >> y;
+
+	if (x * x + y * y < 1 || (x > 0 && x < 1 && y > 0 && y < 1))
+	{
+		cout << "YES" << endl;
+	}
+	else
+	{
+		cout << "NO" << endl;
+	}
+}
+
